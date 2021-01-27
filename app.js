5a5208c1a9bf43dc9edc8f2e2a6e0ffd03ca1039
seokwoo-park <p.seokwoo@gmail.com>
2021-01-27 16:23:00 +0900

    add Stroke

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-const canvas = document.querySelector('#jsCanvas');
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+// canvas.width = document.querySelector('.canvas');
+// canvas.height = document.querySelector('.canvas');
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+console.log(document.getElementsByClassName("canvas"))
+
+// canvas.width = 700;
+// canvas.height = 700;
+
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,24 +22,26 @@ function stopPainting () {
     painting = false;
 }
 
-function onMouseMove(e){
-    const x = e.offsetX;
-    const y = e.offsetY;
-
-}
-
-function onMouseDown(e){
+function startPainting () {
     painting = true;
 }
 
-function onMouseUp(e){
-    painting = false;
+function onMouseMove(e){
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    } else{
+        ctx.lineTo(x,y);
+        ctx.stroke();
+    }
 }
 
 
 if (canvas) {
     canvas.addEventListener('mousemove',onMouseMove);
-    canvas.addEventListener('mousedown',onMouseDown);
-    canvas.addEventListener('mouseup',onMouseUp);
+    canvas.addEventListener('mousedown',startPainting);
+    canvas.addEventListener('mouseup',stopPainting);
     canvas.addEventListener('mouseleave',stopPainting);
 }
